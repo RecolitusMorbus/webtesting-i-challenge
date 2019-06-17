81959b8158d9735820d88cce7cf5b6f5d894311e
Nolan Allen <nolan.r.allen@gmail.com>
2019-06-17 15:08:41 -0500

    Initial commit: project initialized

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,45 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
-}
+  if (item.enhancement < 20) {
+    const itemSuccess = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement
+    };
+    return itemSuccess;
+  } else {
+    return item;
+  };
+};
 
 function fail(item) {
-  return { ...item };
-}
+  if (item.enhancement < 15) {
+    const itemFail = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement
+    };
+    return itemFail;
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    const enhancFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement
+    };
+    return enhancFail;
+  } else if (item.enhancement > 16) {
+    const critFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1
+    };
+    return critFail;
+  };
+};
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
